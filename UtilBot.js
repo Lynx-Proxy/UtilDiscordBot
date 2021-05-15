aa7925a425d3d3e0404c3f1bfc233f8a71ee60c2
Greg Salifu <salifugreg@gmail.com>
2021-05-15 14:31:39 +0000

    Main

--- UtilBot.js
+++ UtilBot.js
@@ -1,1 +1,8 @@
 const path = require('path')
+const fs = require('fs')
+const discord = require('discord.js')
+const bot  = new discord.Client()
+const config = require('./Config.json')
+
+
+bot.login(config.token)
