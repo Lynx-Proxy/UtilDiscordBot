c06ec1c7d0a0eddea3e03471e15714620dbf4c80
Greg Salifu <salifugreg@gmail.com>
2021-05-19 15:52:27 +0000

    added welcome message (not tested)

--- UtilBot.js
+++ UtilBot.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const discord = require('discord.js')
 const bot  = new discord.Client()
 const config = require('./Config.json')
+const { brotliCompress } = require('zlib')
 
 bot.on('ready', async () => {
     console.log(`Bot ready as ${bot.user.username}`)
@@ -10,7 +11,7 @@ bot.on('ready', async () => {
     const handler = 'commandh.js'
     const commandh = require(`./Commands/${handler}`)
 
-    function readcmds(dir){
+    function readcmds(dir){         //Reads Command Files
         const files = fs.readdirSync(path.join(__dirname, dir))
         for (const file of files) {
             const stat = fs.lstatSync(path.join(__dirname, dir, file))
@@ -20,11 +21,22 @@ bot.on('ready', async () => {
             else if (file !== handler){
                 const options = require(path.join(__dirname,dir,file))
                 commandh(bot,options)
+                console.log("read files")
             }
         }
     }
 
     readcmds('Commands')
+})
+
+bot.on('guildMemberAdd',(member) => {
+    bot.channels.cache.get('813785742538047539').then((channel) => {
+        member.displayAvatarURL()
+        var em = new discord.MessageEmbed()
+        .setTitle(`Welcome to ${member.guild.name} please Read ${member.guild.channels.cache.get(813787210703699968).toString()}`)
+        .setImage(member.displayAvatarURL())
+        channel.send(em)
+    })
 })
 
 bot.login(config.token)
